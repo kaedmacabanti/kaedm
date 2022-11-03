c38f8905b18b0706c34cc8ae72afd178eea39a96
kaedmacabanti20 <kaedmacabanti.student@ua.edu.ph>
2022-11-03 20:56:44 +0800

    added image

--- my-app/components/home.js
+++ my-app/components/home.js
@@ -1,39 +1,19 @@
 import React from 'react'
-import profile from '../img/profile.jpg'
+import Image from 'next/image'
+import profile from '../public/img/profile.png'
 
 
-const Image = () => {
+const MyImage = () => {
     return(
     <section className='container1'>
-        <div className='backgroundPhoto'>
-            
-        </div>
-      
+        <Image src={profile} alt='/' className='backgroundPhoto' />
     </section>
     )
 }
 
-const AboutMe = () => {
-    return(
-    <section className='container2'>
-        <section className='mycontent'>
-            <h3>Web Developer</h3>
-            <h2>Kyle Ash Ezequiel D. Macabanti</h2>
-            <p className="intro">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam sint a, repudiandae esse cumque suscipit explicabo, ratione obcaecati cupiditate error est dignissimos vitae eveniet voluptatem, illum corporis fuga quisquam labore?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ullam consectetur ipsa eius molestiae veniam accusamus quia consequatur fugiat ipsum! Quibusdam consequuntur sunt necessitatibus asperiores quas eveniet recusandae ratione numquam!
-            </p>
-            <hr/>
-            <div className="info templateCollumn">
-                        <div> <b>Birthday:</b>  November 20 2001</div>
-                        <div> <b>Degree:</b> Bachelor of Science in Information Technology</div>
-                        <div> <b>Website:</b> wwww.kaedm.com</div>
-                        <div> <b>Email: </b>kyleashderaco@gmail.com</div>
-                        <div> <b>Contact Number:</b> 0926 362 0950</div>
-                        <div> <b>LinkedIn:</b> KyleAshMacabanti</div>
-            </div>
-            <hr/>   
-
+const Services = () => {
+  return (
+    <div className='mycontent'>
             <h3>What I do</h3>
             <h2>Services</h2>
             <div class="skillInfo templateCollumn">
@@ -62,8 +42,27 @@ const AboutMe = () => {
                         facere labore, vel, aliquid est magnam assumenda,
                         hic esse nesciunt quia. Enim maiores nesciunt in</div>
                 </div>
+      
+    </div>
+  )
+}
+
+
+const AboutMe = () => {
+    return(
+    <section className='container2'>
+
+        <Image src={profile} alt='/' className='backgroundPhoto' />  
+   
+        <section className='mycontentintroduction'>
+            {/* <h3>Front-End Web Developer</h3> */}
+            <h2>Hi, I'm Kyle Ash Ezequiel D. Macabanti a front-end developer.</h2>
+       
         </section>
+        <br/>
+        <br/>
     </section>
+
     )
 }
 
@@ -72,8 +71,9 @@ const AboutMe = () => {
 const KaedHome = () => {
   return (
     <section className='maincontainer'>
-        <Image/>
+       
         <AboutMe/>
+        <Services/>
     </section>
   )
 }
