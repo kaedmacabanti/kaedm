b59accdfb91c1b0e4e74858a1f6f85d5454c6648
kaedmacabanti20 <kaedmacabanti.student@ua.edu.ph>
2022-11-03 18:29:26 +0800

    added details

---
+++ my-app/components/resume.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div className="mycontent">
+            <h3>Resume</h3>
+            <h2>Education</h2>
+            <div className="rContainer">
+                <div><h4>University Of the Assumption</h4><p>Bachelor of Science in Information Technology</p> </div>
+                <div><h4>Adelle Grace Montesori</h4> <p>Senior High School: Science Technology Engineering and Mathematics</p> </div>
+                <div><h4>Justino Sevilla High School</h4><p>Junior High School: Science Technology Engineering</p> </div>
+            </div>
+            <hr></hr>
+            <h2>Awards and achievements</h2>
+            <div class="rContainer">
+                <div><h4>First Place: HGU Social Innovation</h4><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta reiciendis omnis autem aut harum repellat ut eaque dolore commodi deserunt necessitatibus saepe itaque, numquam odit iure consequuntur reprehenderit quos!</p></div>
+                <div><h4>UDemy Certification</h4> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor natus molestiae repudiandae. Nisi necessitatibus sed laboriosam possimus blanditiis repudiandae, tempore molestias, soluta modi obcaecati cumque illum dicta. In, veritatis officia.</p></div>
+                <div><h4>FreeCodeCamp Certification</h4> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quibusdam, laborum aut quo fuga eum aspernatur dolore temporibus suscipit, alias sunt vero id veritatis quaerat. Iusto in modi asperiores ipsa?</p></div>
+            </div>
+            
+    </div>
+  )
+}
+
+export default Resume
