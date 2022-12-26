7e18d56c990a257b14ef7403b5fa84c70d3b670e
kaedmacabanti20 <kaedmacabanti.student@ua.edu.ph>
2022-12-26 17:02:16 +0800

    footer update

--- my-app/components/footer.js
+++ my-app/components/footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import li from '../public/img/linkedin.png'
+import logo from '../public/img/white.png'
+import Image from 'next/image'
+
 
 const Footer = () => {
   return (
     <section className='footer'>
-      
+      <div>
+        <Image src={logo} alt='/' className='navlogo' />
+        <p>Resume</p>
+      </div> 
+      <div>
+        <Image src={li} alt='/' className='navlogo' />
+        <p>LinkedIn</p>
+      </div> 
     </section>
   )
 }
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -27,9 +27,9 @@ export default function Home() {
         <Featuredproj/>
         <Projects/>
         <Contact/> 
+      </div> 
 
-      </div>
-
+      
       <Footer/>
 
     </body>
